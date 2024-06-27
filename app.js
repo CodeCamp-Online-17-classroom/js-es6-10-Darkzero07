4e05c4454e757c084af4323af909fe9e277f2a66
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-27 23:45:16 +0700

    js lab 154

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+// จงเขียนฟังก์ชัน cloneObject โดยรับพารามิเตอร์มาเป็น Object และ return ค่าเป็น Object ที่มี key-value เหมือนกับพารามิเตอร์ที่รับเข้ามาทุกตัว
+
+function cloneObject(obj) {
+    return {...obj}
+}
+
+const originalObject = { a: 1, b: 2, c: 3 };
+const clonedObject = cloneObject(originalObject);
+
+console.log(originalObject); // { a: 1, b: 2, c: 3 }
+console.log(clonedObject); // { a: 1, b: 2, c: 3 }
